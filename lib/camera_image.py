3862c141332d62afb5be0c92d25d9c1c6ab26428
Xueping Long <gregllong@gmail.com>
2016-06-02 21:58:01 -0700

    made an object that takes in an image array from the camera and fit to two 1D gaussian functions (column sum and row sum)
    made a test file for this object. Tests pass

---
+++ lib/camera_image.py
@@ -0,0 +1,63 @@
+"""
+Class to hold row and column sum data, projections of the image.
+"""
+import numpy as _n
+# import cv2 as _cv2
+import functions as _functions
+
+
+class CameraImage(object):
+    """ Processed single image taken by the pi-camera. """
+    def __init__(self, image, coarsen=False):
+        self.image = image
+        self.coarsen = coarsen
+
+        self._global_max = _n.max(self.image)
+        self._scale_factor = 40.
+        self._beam_diameter_guess = 200.
+
+        self._process_image()
+
+    def _process_image(self):
+        self._calculate_column_and_row_sums()
+        if self.coarsen:
+            self._coarsen()
+        self._make_guesses()
+        self._gaussian_fit_column_and_row_sums()
+
+    def _calculate_column_and_row_sums(self):
+        column_sum = self.image.sum(axis=1)/self._scale_factor
+        column_sum = column_sum[::-1]
+
+        row_sum = self.image.sum(axis=0)/self._scale_factor
+
+        # background substraction
+        self.column_sum = column_sum - _n.min(column_sum)
+        self.row_sum = row_sum - _n.min(row_sum)
+
+        self.column_positions = range(len(self.column_sum))
+        self.row_positions = range(len(self.row_sum))
+
+    def _coarsen(self):
+        self._coarsen_factor = 3.
+        # TODO: add coarsen functionality if necessary
+        pass
+
+    def _make_guesses(self):
+        self._column_amp_guess = self.column_sum.max()
+        self._column_center_guess = _n.argmax(self.column_sum)
+        self._row_amp_guess = self.row_sum.max()
+        self._row_center_guess = _n.argmax(self.row_sum)
+
+    def _gaussian_fit_column_and_row_sums(self):
+        column_fit = _functions.fit_gaussian(
+            self.column_positions, self.column_sum, self._column_amp_guess,
+            self._column_center_guess, self._beam_diameter_guess)
+
+        self.column_amp, self.column_center, self.column_width = column_fit
+
+        row_fit = _functions.fit_gaussian(
+            self.row_positions, self.row_sum, self._row_amp_guess,
+            self._row_center_guess, self._beam_diameter_guess)
+
+        self.row_amp, self.row_center, self.row_width = row_fit
---
+++ tests/lib/test_camera_image.py
@@ -0,0 +1,58 @@
+"""
+Test camera_image.py module
+
+Preparation of the 2D gaussian data is taken from the following link:
+
+http://stackoverflow.com/questions/21566379/
+fitting-a-2d-gaussian-function-using-scipy-optimize-curve-fit-valueerror-and-m
+"""
+import unittest as ut
+import pi_beam_profiler.lib.camera_image as c
+import numpy as np
+
+
+class TestMath(ut.TestCase):
+    def setUp(self):
+        image = self._prepare_image()
+        self.camera_image = c.CameraImage(image=image)
+
+    def tearDown(self):
+        self.camera_image = None
+
+    def test_row_values(self):
+        row_amp = self.camera_image.row_amp
+        row_center = self.camera_image.row_center
+        row_width = self.camera_image.row_width
+        exp_row_amp = 6.64670194
+        exp_row_center = 300.
+        exp_row_width = 30.
+        self.assertAlmostEquals(row_center, exp_row_center)
+        self.assertAlmostEquals(row_width, exp_row_width)
+        self.assertAlmostEquals(row_amp, exp_row_amp)
+
+    def test_column_values(self):
+        column_amp = self.camera_image.column_amp
+        column_center = self.camera_image.column_center
+        column_width = self.camera_image.column_width
+        exp_column_amp = 3.988021164
+        exp_column_center = 300.
+        exp_column_width = 50.
+        self.assertAlmostEquals(column_center, exp_column_center)
+        self.assertAlmostEquals(column_width, exp_column_width)
+        self.assertAlmostEquals(column_amp, exp_column_amp)
+
+    def _prepare_image(self):
+        x = np.linspace(0, 600, 601)
+        y = np.linspace(0, 600, 601)
+        x, y = np.meshgrid(x, y)
+        data = self._twoD_Gaussian((x, y), 3, 300, 300, 30, 50)
+        image = data.reshape(601, 601)
+        return image
+
+    def _twoD_Gaussian(self, (x, y), amplitude, xo, yo, sigma_x, sigma_y):
+        xo = float(xo)
+        yo = float(yo)
+        a = 1./(sigma_x**2)
+        c = 1./(sigma_y**2)
+        g = amplitude*np.exp(-(a*((x-xo)**2) + c*((y-yo)**2)))
+        return g.ravel()
