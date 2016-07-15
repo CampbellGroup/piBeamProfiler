4822c9b08bd0e8aadebcc620fdfdbe6132b9cc34
Xueping Long <gregllong@gmail.com>
2016-07-15 14:19:17 -0700

    scaling might work

--- lib/gui.py
+++ lib/gui.py
@@ -302,7 +302,8 @@ class CameraDisplay(QtGui.QLabel):
         gray_image = np.require(array, np.uint8, 'C')
         q_image = QtGui.QImage(gray_image.data, w, h,
                                QtGui.QImage.Format_Indexed8)
-        qPixmap = QtGui.QPixmap(q_image)#.scaled(self.width, self.height))
+        qPixmap = QtGui.QPixmap(q_image)
+        qPixmap.scaled(self.width, self.height)
         return qPixmap
 
 
